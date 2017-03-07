46d549e929aa205e16b3738cf756012a9f339d3e
Vasily Kolesnikov <re.vkolesnikov@gmail.com>
2017-03-07 13:20:24 +0300

    Add basic functions
     - Numbers: ZERO, ONE, TWO
     - Boolean: TRUE, FALSE, IF_ELSE
     - Pair data structure
     - Arithmetic: INC, DEC, ADD, SUB, MUL, POW

--- lib/untyped_lambda_calculus.rb
+++ lib/untyped_lambda_calculus.rb
@@ -1,4 +1,26 @@
 require "untyped_lambda_calculus/version"
 
 module UntypedLambdaCalculus
+  ZERO = ->(*) { ->(x) { x } }
+  ONE  = ->(p) { ->(x) { p[x] } }
+  TWO  = ->(p) { ->(x) { p[p[x]] } }
+
+  TRUE  = ->(x) { ->(*) { x } }
+  FALSE = ->(*) { ->(y) { y } }
+
+  IF = ->(p) { ->(x) { ->(y) { p[x][y] } } }
+
+  PAIR  = ->(x) { ->(y) { ->(p) { p[x][y] } } }
+  LEFT  = ->(p) { p[->(x) { ->(*) { x } }] }
+  RIGHT = ->(p) { p[->(*) { ->(y) { y } }] }
+
+  SLIDE = ->(p) { PAIR[RIGHT[p]][INC[RIGHT[p]]] }
+
+  INC = ->(n) { ->(p) { ->(x) { p[n[p][x]] } } }
+  DEC = ->(n) { LEFT[n[SLIDE][PAIR[ZERO][ZERO]]] }
+
+  ADD = ->(n) { ->(m) { m[INC][n] } }
+  SUB = ->(n) { ->(m) { m[DEC][n] } }
+  MUL = ->(n) { ->(m) { m[ADD[n]][ZERO] } }
+  POW = ->(n) { ->(m) { m[MUL[n]][ONE]  } }
 end
--- spec/untyped_lambda_calculus_spec.rb
+++ spec/untyped_lambda_calculus_spec.rb
@@ -1,4 +1,78 @@
-require "spec_helper"
+require 'spec_helper'
 
-RSpec.describe UntypedLambdaCalculus do
+# rubocop:disable Metrics/BlockLength
+module UntypedLambdaCalculus
+  RSpec.describe UntypedLambdaCalculus do
+    let(:to_integer) { ->(p) { p[->(x) { x + 1 }][0] } }
+    let(:to_boolean) { ->(p) { p[true][false] } }
+
+    describe 'to_integer' do
+      it { expect(to_integer[ZERO]).to eq 0 }
+      it { expect(to_integer[ONE]).to eq 1 }
+    end
+
+    describe 'to_boolean' do
+      it { expect(to_boolean[TRUE]).to eq true }
+      it { expect(to_boolean[FALSE]).to eq false }
+    end
+
+    describe 'if' do
+      it { expect(to_boolean[IF[TRUE][TRUE][TRUE]]).to eq true }
+      it { expect(to_boolean[IF[TRUE][TRUE][FALSE]]).to eq true }
+      it { expect(to_boolean[IF[TRUE][FALSE][TRUE]]).to eq false }
+      it { expect(to_boolean[IF[TRUE][FALSE][FALSE]]).to eq false }
+
+      it { expect(to_boolean[IF[FALSE][TRUE][TRUE]]).to eq true }
+      it { expect(to_boolean[IF[FALSE][TRUE][FALSE]]).to eq false }
+      it { expect(to_boolean[IF[FALSE][FALSE][TRUE]]).to eq true }
+      it { expect(to_boolean[IF[FALSE][FALSE][FALSE]]).to eq false }
+    end
+
+    describe 'increment' do
+      it { expect(to_integer[INC[ONE]]).to eq 2 }
+      it { expect(to_integer[INC[TWO]]).to eq 3 }
+    end
+
+    describe 'decrement' do
+      it { expect(to_integer[DEC[ONE]]).to eq 0 }
+      it { expect(to_integer[DEC[TWO]]).to eq 1 }
+    end
+
+    describe 'pair' do
+      let(:p) { PAIR[ONE][TWO] }
+      it { expect(to_integer[LEFT[p]]).to eq 1 }
+      it { expect(to_integer[RIGHT[p]]).to eq 2 }
+    end
+
+    describe 'ADD' do
+      it { expect(to_integer[ADD[ZERO][ONE]]).to eq 1 }
+      it { expect(to_integer[ADD[ZERO][TWO]]).to eq 2 }
+      it { expect(to_integer[ADD[ONE][ONE]]).to eq 2 }
+      it { expect(to_integer[ADD[ONE][TWO]]).to eq 3 }
+      it { expect(to_integer[ADD[TWO][TWO]]).to eq 4 }
+    end
+
+    describe 'SUB' do
+      it { expect(to_integer[SUB[ONE][ONE]]).to eq 0 }
+      it { expect(to_integer[SUB[TWO][ONE]]).to eq 1 }
+      it { expect(to_integer[SUB[TWO][TWO]]).to eq 0 }
+      it { expect(to_integer[SUB[ONE][ZERO]]).to eq 1 }
+      it { expect(to_integer[SUB[TWO][ZERO]]).to eq 2 }
+      it { expect(to_integer[SUB[ZERO][ZERO]]).to eq 0 }
+    end
+
+    describe 'MUL' do
+      it { expect(to_integer[MUL[ZERO][ZERO]]).to eq 0 }
+      it { expect(to_integer[MUL[ONE][ZERO]]).to eq 0 }
+      it { expect(to_integer[MUL[TWO][ZERO]]).to eq 0 }
+      it { expect(to_integer[MUL[ONE][TWO]]).to eq 2 }
+      it { expect(to_integer[MUL[TWO][TWO]]).to eq 4 }
+    end
+
+    describe 'POW' do
+      it { expect(to_integer[POW[ONE][ONE]]).to eq 1 }
+      it { expect(to_integer[POW[TWO][ONE]]).to eq 2 }
+      it { expect(to_integer[POW[TWO][TWO]]).to eq 4 }
+    end
+  end
 end
