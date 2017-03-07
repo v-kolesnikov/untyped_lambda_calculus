7e82422709a462cd8dc7d48ff40b5028d5e69753
Vasily Kolesnikov <re.vkolesnikov@gmail.com>
2017-03-07 13:20:24 +0300

    Add basic functions
     - Numbers: ZERO, ONE, TWO
     - Boolean: TRUE, FALSE, IF_ELSE
     - Pair data structure
     - Arithmetic: INC, DEC, ADD, SUB, MUL, POW

--- lib/untyped_lambda_calculus.rb
+++ lib/untyped_lambda_calculus.rb
@@ -1,4 +1,26 @@
 require "untyped_lambda_calculus/version"
 
 module UntypedLambdaCalculus
+  ZERO = ->(*) { ->(x) { x } }
+  ONE  = ->(p) { ->(x) { p[x] } }
+  TWO  = ->(p) { ->(x) { p[p[x]] } }
+
+  TRUE  = ->(x) { ->(*) { x } }
+  FALSE = ->(*) { ->(y) { y } }
+
+  IF = ->(p) { ->(x) { ->(y) { p[x][y] } } }
+
+  PAIR  = ->(x) { ->(y) { ->(p) { p[x][y] } } }
+  LEFT  = ->(p) { p[->(x) { ->(*) { x } }] }
+  RIGHT = ->(p) { p[->(*) { ->(y) { y } }] }
+
+  SLIDE = ->(n) { ->(x) { n[PAIR[LEFT[x]][INC[RIGHT[x]]]] } }
+
+  INC = ->(n) { ->(p) { ->(x) { p[n[p][x]] } } }
+  DEC = ->(n) { LEFT[SLIDE[n][PAIR[ZERO][ZERO]]] }
+
+  ADD = ->(n) { ->(m) { m[INC][n] } }
+  SUB = ->(n) { ->(m) { m[DEC][n] } }
+  MUL = ->(n) { ->(m) { m[ADD[n]][ZERO] } }
+  POW = ->(n) { ->(m) { m[MUL[n]][ONE]  } }
 end
